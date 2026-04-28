81dfb3e4ea691a7596797a74c72d6f965f01448b
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-28 13:03:42 +0700

    Menambah kondisi insert di awal saat list kosong

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -32,5 +32,7 @@ public:
 
         // Step 2: Assign value to the data fields 
         newNode->noMhs = nim;
+
+         // Step 3 : Insert at beginning if list is empty or nim is smallest
+        if (START == NULL || nim <= START->noMhs)
     }
-};
