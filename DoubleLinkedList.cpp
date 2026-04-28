24aba780c5a05bfa4e0d6426e499f1fd5e84f062
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-28 13:13:19 +0700

    Implementasi node baru di tengah double linked list

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -71,6 +71,16 @@ public:
             return;
         }
 
+        // Step 9: Insert between current and current->next 
+        newNode->next = current->next; //Step 9a: newNode.next = current.next
+        newNode->prev = current;      //Step 9a: newNode.next = current
+
+        // insert last node 
+        if (current->next != NULL)
+            current->next->prev = newNode; //Step 9c: current.next = newNode
+    }
+
+
 
 
 
